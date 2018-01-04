11cdb20a3e7f1eefa331cec00c8d14671845e6be
Sherab Giovannini <sherab.giovannini@byteheed.com>
2018-01-04 11:56:17 +0100

    Added a stealth-interception test.
    REFACTOR: Using debug! with logger instead of println!.

--- src/memguard/tests.rs
+++ src/memguard/tests.rs
@@ -88,9 +88,44 @@ pub fn tests(matches: &ArgMatches, logger: Logger) {
 fn interception_tests(matches: &ArgMatches, logger: Logger) {
     match matches.subcommand() {
         ("kernel",      Some(matches))  => test_intercept_kernel_region(matches, logger),
+        ("stealth",     Some(matches))  => test_stealth_interception(matches, logger),
         ("callback",    Some(matches))  => test_interception_callback(matches, logger),
         _                                 => println!("{}", matches.usage())
     }
+}
+
+//
+// This test aims to demostrate that we are able to ignore any write to any memory address
+//
+fn test_stealth_interception(_matches: &ArgMatches, logger: Logger) {
+    let partition: Partition = Partition::root();
+    let mut guard = Guard::new(&partition);
+
+    const POOL_SIZE: usize = 0x100;
+
+    debug!(logger, "allocating pool");
+    let addr = memory::alloc_virtual_memory(&partition.device, POOL_SIZE);
+
+    debug!(logger, "addr: 0x{:016x}", addr);
+
+    let region = Sentinel::region(&partition, addr, POOL_SIZE as u64, Access::WRITE);
+
+    debug!(logger, "adding {} to {}", region, guard);
+    guard.add(region);
+
+    guard.set_callback(Box::new(|interception| {
+        println!("Attemp to write at 0x{:016X} - IGNORING", interception.address);
+        Action::STEALTH
+    }));
+
+    debug!(logger, "starting guard");
+    guard.start();
+    debug!(logger, "accessing memory 0x{:016x}", addr);
+    let _ = memory::read_virtual_memory(&partition.device, addr, POOL_SIZE);
+    debug!(logger, "stoping guard");
+    guard.stop();
+
+    memory::free_virtual_memory(&partition.device, addr);
 }
 
 
@@ -101,20 +136,20 @@ fn _callback_test(interception: Interception) -> Action {
     Action::CONTINUE
 }
 
-fn test_interception_callback(_matches: &ArgMatches, _logger: Logger) {
+fn test_interception_callback(_matches: &ArgMatches, logger: Logger) {
     let partition: Partition = Partition::root();
     let mut guard = Guard::new(&partition);
 
     const POOL_SIZE: usize = 0x100;
 
-    println!("allocating pool");
+    debug!(logger, "allocating pool");
     let addr = memory::alloc_virtual_memory(&partition.device, POOL_SIZE);
 
-    println!("addr: 0x{:016x}", addr);
+    debug!(logger, "addr: 0x{:016x}", addr);
 
     let region = Sentinel::region(&partition, addr, POOL_SIZE as u64, Access::READ);
 
-    println!("adding {} to {}", region, guard);
+    debug!(logger, "adding {} to {}", region, guard);
     guard.add(region);
 
     // guard.set_callback(Box::new(_callback_test));
@@ -124,11 +159,11 @@ fn test_interception_callback(_matches: &ArgMatches, _logger: Logger) {
 
         Action::CONTINUE
     }));
-    println!("starting guard");
+    debug!(logger, "starting guard");
     guard.start();
-    println!("accessing memory 0x{:016x}", addr);
+    debug!(logger, "accessing memory 0x{:016x}", addr);
     let _ = memory::read_virtual_memory(&partition.device, addr, POOL_SIZE);
-    println!("stoping guard");
+    debug!(logger, "stoping guard");
     guard.stop();
 
     memory::free_virtual_memory(&partition.device, addr);
@@ -251,20 +286,22 @@ fn test_virtual_memory(_matches: &ArgMatches, logger: Logger) {
     memory::free_virtual_memory(&device, addr);
 }
 
-fn test_memory_write(_matches: &ArgMatches, _logger: Logger) {
+fn test_memory_write(_matches: &ArgMatches, logger: Logger) {
     let device = Device::new(core::SE_NT_DEVICE_NAME);
+    debug!(logger, "reading virtual memory");
     let v = memory::read_virtual_memory(&device, KERNEL_ADDR, 0x200);
 
+    debug!(logger, "writting virtual memory");
     memory::write_virtual_memory(&device, KERNEL_ADDR, v);
 }
 
 
-fn test_memory_map(_matches: &ArgMatches, _logger: Logger) {
+fn test_memory_map(_matches: &ArgMatches, logger: Logger) {
     let device = Device::new(core::SE_NT_DEVICE_NAME);
 
     let map = Map::new(&device, KERNEL_ADDR, 0x200);
 
-    println!("map: {:?}", map);
+    debug!(logger, "map: {:?}", map);
 
 }
 
@@ -322,32 +359,32 @@ fn test_read_eprocess(_matches: &ArgMatches, logger: Logger) {
 // TODO: Find a more generic kernel pointer
 const KERNEL_ADDR: u64 = 0xfffffa800231e9e0;
 
-fn create_multiple_partitions(_logger: Logger) {
-    println!("creating 3 partitions");
+fn create_multiple_partitions(logger: Logger) {
+    debug!(logger, "creating 3 partitions");
     let _partition1: Partition = Partition::new();
     let _partition2: Partition = Partition::new();
     let _partition3: Partition = Partition::new();
-    println!("waiting 5 seconds");
+    debug!(logger, "waiting 5 seconds");
     thread::sleep(Duration::from_secs(5));
-    println!("done, destroying partitions");
+    debug!(logger, "done, destroying partitions");
 }
 
-fn create_partition(_logger: Logger) {
+fn create_partition(logger: Logger) {
     let partition: Partition = Partition::root();
-    println!("created partition: {:?}", partition);
-    println!("waiting 5 seconds");
+    debug!(logger, "created partition: {:?}", partition);
+    debug!(logger, "waiting 5 seconds");
     thread::sleep(Duration::from_secs(5));
-    println!("done, destroying partition");
+    debug!(logger, "done, destroying partition");
 }
 
 pub fn partition(matches: &ArgMatches, logger: Logger) {
     match matches.subcommand() {
-        ("create",  Some(_))  => create_partition(logger),
+        ("create",  Some(_))           => create_partition(logger),
         ("create-multiple",  Some(_))  => create_multiple_partitions(logger),
-        ("delete",  Some(_))  => _not_implemented_command(logger),
-        ("getinfo", Some(_))  => _not_implemented_command(logger),
-        ("setinfo", Some(_))  => _not_implemented_command(logger),
-        _                     => println!("{}", matches.usage())
+        ("delete",  Some(_))           => _not_implemented_command(logger),
+        ("getinfo", Some(_))           => _not_implemented_command(logger),
+        ("setinfo", Some(_))           => _not_implemented_command(logger),
+        _                              => println!("{}", matches.usage())
     }
 }
 
@@ -364,25 +401,25 @@ fn guard_tests(matches: &ArgMatches, logger: Logger) {
 }
 
 
-fn start_guard_a_second(guard: &Guard) {
-    println!("starting {}", guard);
+fn start_guard_a_second(guard: &Guard, logger: &Logger) {
+    debug!(logger, "starting {}", guard);
     guard.start();
 
     let duration = Duration::from_secs(1);
-    println!("waiting {:?}", duration);
+    debug!(logger, "waiting {:?}", duration);
     thread::sleep(duration);
 
-    println!("stopping {}", guard);
+    debug!(logger, "stopping {}", guard);
     guard.stop();
 }
-fn start_a_guard(_matches: &ArgMatches, _logger: Logger) {
+fn start_a_guard(_matches: &ArgMatches, logger: Logger) {
     let partition: Partition = Partition::root();
     let guard = Guard::new(&partition);
 
-    start_guard_a_second(&guard);
+    start_guard_a_second(&guard, &logger);
 }
 
-fn create_multiple_guards(_matches: &ArgMatches, _logger: Logger) {
+fn create_multiple_guards(_matches: &ArgMatches, logger: Logger) {
     let partition: Partition = Partition::root();
     let _guard = Guard::new(&partition);
 
@@ -391,16 +428,16 @@ fn create_multiple_guards(_matches: &ArgMatches, _logger: Logger) {
             guard
         }).collect();
 
-    println!("guards-created: {}", guards.len());
+    debug!(logger, "guards-created: {}", guards.len());
 
-    println!("enumerate-guards");
+    debug!(logger, "enumerate-guards");
 
     // for guard in Guard::enumerate() {
     //     println!("guard: {}", guards);
     // }
 
     for guard in guards {
-        println!("{}", guard);
+        debug!(logger, "{}", guard);
     }
 }
 
@@ -410,13 +447,12 @@ fn create_multiple_guards(_matches: &ArgMatches, _logger: Logger) {
 //
 fn region_tests(matches: &ArgMatches, logger: Logger) {
     match matches.subcommand() {
-        ("create", Some(matches))  => test_create_region(matches, logger),
-        ("enumerate", Some(matches))  => test_enumerate_region(matches, logger),
-        ("create-multiple", Some(matches))  => test_create_multiple_regions(matches, logger),
-        ("intercept", Some(matches))  => test_intercept_region(matches, logger),
-        ("kernel-intercept", Some(matches))  => test_intercept_kernel_region(matches, logger),
-        ("regions-inside-guard", Some(matches))  => test_regions_inside_guard(matches, logger),
-        _                          => println!("{}", matches.usage())
+        ("create",               Some(matches)) => test_create_region(matches, logger),
+        ("enumerate",            Some(matches)) => test_enumerate_region(matches, logger),
+        ("create-multiple",      Some(matches)) => test_create_multiple_regions(matches, logger),
+        ("intercept",            Some(matches)) => test_intercept_region(matches, logger),
+        ("regions-inside-guard", Some(matches)) => test_regions_inside_guard(matches, logger),
+        _                                       => println!("{}", matches.usage())
     }
 }
 
@@ -424,16 +460,16 @@ fn test_enumerate_region(_matches: &ArgMatches, _logger: Logger) {
     unimplemented!()
 }
 
-fn test_create_multiple_regions(_matches: &ArgMatches, _logger: Logger) {
+fn test_create_multiple_regions(_matches: &ArgMatches, logger: Logger) {
     let partition: Partition = Partition::root();
     let _regions: Vec<Sentinel> = (0..10).map(|_| {
             let region = Sentinel::region(&partition, 0xCAFEBABE, 0x1000, Access::READ);
-            println!("{}", region);
+            debug!(logger, "{}", region);
             region
         }).collect();
 }
 
-fn test_regions_inside_guard(_matches: &ArgMatches, _logger: Logger) {
+fn test_regions_inside_guard(_matches: &ArgMatches, logger: Logger) {
 
     let partition: Partition = Partition::root();
 
@@ -449,67 +485,66 @@ fn test_regions_inside_guard(_matches: &ArgMatches, _logger: Logger) {
         guard.add(region);
     }
 
-
-    start_guard_a_second(&guard);
+    start_guard_a_second(&guard, &logger);
 }
 
-fn test_intercept_kernel_region(_matches: &ArgMatches, _logger: Logger) {
+fn test_intercept_kernel_region(_matches: &ArgMatches, logger: Logger) {
     let partition: Partition = Partition::root();
     let mut guard = Guard::new(&partition);
 
     const POOL_SIZE: usize = 0x100;
 
-    println!("allocating pool");
+    debug!(logger, "allocating pool");
     let addr = memory::alloc_virtual_memory(&partition.device, POOL_SIZE);
-    println!("addr: 0x{:016x}", addr);
+    debug!(logger, "addr: 0x{:016x}", addr);
 
     let region = Sentinel::region(&partition, addr, POOL_SIZE as u64, Access::READ);
 
-    println!("adding {} to {}", region, guard);
+    debug!(logger, "adding {} to {}", region, guard);
 
     guard.add(region);
-    println!("starting guard");
+    debug!(logger, "starting guard");
     guard.start();
-    println!("accessing memory 0x{:016x}", addr);
+    debug!(logger, "accessing memory 0x{:016x}", addr);
 
     let _ = memory::read_virtual_memory(&partition.device, addr, POOL_SIZE);
 
-    println!("stoping guard");
+    debug!(logger, "stoping guard");
     guard.stop();
 
     memory::free_virtual_memory(&partition.device, addr);
 }
 
-fn test_intercept_region(_matches: &ArgMatches, _logger: Logger) {
+fn test_intercept_region(_matches: &ArgMatches, logger: Logger) {
     let mut v: Vec<u8> = Vec::new();
     v.push(13);
 
     let partition: Partition = Partition::root();
     let mut guard = Guard::new(&partition);
     let region = Sentinel::region(&partition, v.as_ptr() as u64, 10, Access::READ);
-    println!("adding {} to {}", region, guard);
+    debug!(logger, "adding {} to {}", region, guard);
     guard.add(region);
-    println!("starting guard, and sleeping 5 seconds");
+    debug!(logger, "starting guard, and sleeping 5 seconds");
     guard.start();
     thread::sleep(Duration::from_secs(5));
 
     // accessing memory
-    println!("accessing memory {:?} 5 times", v.as_ptr());
+    debug!(logger, "accessing memory {:?} 5 times", v.as_ptr());
     let _ = v[0];
     let _ = v[0];
     let _ = v[0];
     let _ = v[0];
     let value = v[0];
 
-    println!("value: {}", value);
-    println!("sleeping 5 secs");
+    debug!(logger, "value: {}", value);
+    debug!(logger, "sleeping 5 secs");
     thread::sleep(Duration::from_secs(5));
-    println!("stoping guard");
+    debug!(logger, "stoping guard");
     guard.stop();
 }
 
-fn test_create_region(_matches: &ArgMatches, _logger: Logger) {
+fn test_create_region(_matches: &ArgMatches, logger: Logger) {
     let partition: Partition = Partition::root();
     let region = Sentinel::region(&partition, 0xCAFEBABE, 0x1000, Access::READ);
-    println!("{}", region);
+    debug!(logger, "{}", region);
 }
